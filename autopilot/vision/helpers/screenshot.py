6b0c62c5480a7fc622ec4e5fdf397555aba8125f
SKai <skai2mail@gmail.com>
2020-07-31 20:42:37 -0300

    Tweaks to screenshot

--- autopilot/vision/helpers/screenshot.py
+++ autopilot/vision/helpers/screenshot.py
@@ -9,7 +9,5 @@ def screenshot(left, top, right, bottom):
     return d.screenshot(region=(left, top, right, bottom))
 
 if __name__ == '__main__':
-    print(screenshot(100,100,300,300))
+    print(screenshot(100, 100, 300, 300))
     print(d.display)
-    resolution = d.display.resolution
-    print(resolution[0])
