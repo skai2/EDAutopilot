4372993dd3ef513f968c2b555af6dad2164e52cc
SKai <skai2mail@gmail.com>
2020-07-31 21:06:44 -0300

    Tweaks to doc string

--- autopilot/vision/helpers/screenshot.py
+++ autopilot/vision/helpers/screenshot.py
@@ -1,11 +1,11 @@
-import d3dshot
+import d3dshot # <--- Apparently fastest screenshot method on windows
 import numpy
 
 _d3d = d3dshot.create(capture_output="numpy")
 
 
 def screenshot(left, top, right, bottom):
-    """Gets screenshot specified by given coordinates"""
+    """Gets RGB screenshot in cv2 numpy format specified by given region"""
     left, top, right, bottom = round(left), round(top), round(right), round(bottom)
     # _d3d,create above has output set to numpy but it doesn't seem to care :/ output is pil
     pil_image = _d3d.screenshot(region=(left, top, right, bottom))
