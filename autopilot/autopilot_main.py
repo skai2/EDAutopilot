c06624c75614cd082e2bc1286dc905c4a451f933
SKai <skai2mail@gmail.com>
2020-08-01 19:11:11 -0300

    Final implement, autopilot restructure complete

---
+++ autopilot/autopilot_main.py
@@ -0,0 +1,45 @@
+################################################################################
+#                       Elite Dangerous Autopilot v2
+#    A computer vision based, completely external autopilot module for ED
+################################################################################
+from time import sleep
+
+from autopilot.control import keybinds
+from autopilot.control.directinput import directinput_keys
+from autopilot.control.directinput.send import send
+from autopilot.edlog.ship import ship
+from autopilot.routines.align import align
+from autopilot.routines.jump import jump
+from autopilot.routines.refuel import refuel
+from autopilot.routines.reposition import reposition
+
+keys = keybinds.get_latest()
+keys = directinput_keys.get(keys)
+
+
+def activate():
+    # logging.info('\n'+200*'-'+'\n'+'---- AUTOPILOT START '+179*'-'+'\n'+200*'-')
+    # logging.info('get_latest_log='+str(get_latest_log(PATH_LOG_FILES)))
+    # logging.debug('ship='+str(ship()))
+    #     if ship()['target']:
+    #         undock()
+    while ship()['target']:
+        if ship()['status'] == 'in_space' or ship()['status'] == 'in_supercruise':
+            # logging.info('\n'+200*'-'+'\n'+'---- AUTOPILOT ALIGN '+179*'-'+'\n'+200*'-')
+            align()
+            # logging.info('\n'+200*'-'+'\n'+'---- AUTOPILOT JUMP '+180*'-'+'\n'+200*'-')
+            jump()
+            # logging.info('\n'+200*'-'+'\n'+'---- AUTOPILOT REFUEL '+178*'-'+'\n'+200*'-')
+            refueled = refuel()
+            # logging.info('\n'+200*'-'+'\n'+'---- AUTOPILOT POSIT '+179*'-'+'\n'+200*'-')
+            if refueled:
+                reposition(refueled_multiplier=4)
+            else:
+                reposition(refueled_multiplier=1)
+    send(keys['SetSpeedZero'])
+    # logging.info('\n'+200*'-'+'\n'+'---- AUTOPILOT END '+181*'-'+'\n'+200*'-')
+
+
+if __name__ == '__main__':
+    sleep(5)
+    activate()
