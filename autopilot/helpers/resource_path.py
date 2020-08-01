4d5f19ad3a45519f79ee21630ed49dc76808d594
SKai <skai2mail@gmail.com>
2020-08-01 13:26:48 -0300

    New general helpers

---
+++ autopilot/helpers/resource_path.py
@@ -0,0 +1,17 @@
+import sys
+from os.path import abspath, join
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = abspath(".")
+
+    return join(base_path, relative_path)
+
+
+if __name__ == '__main__':
+    print(resource_path('.'))
